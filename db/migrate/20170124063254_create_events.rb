83884af59ab6fea12ee9710d71b287711e760076
Mihael <mkgb7788@example.com>
2017-01-24 14:38:46 +0800

    create table

---
+++ db/migrate/20170124063254_create_events.rb
@@ -0,0 +1,11 @@
+class CreateEvents < ActiveRecord::Migration[5.0]
+  def change
+    create_table :events do |t|
+      t.string :teamName
+      t.text :description
+      t.boolean :is_west
+
+      t.timestamps
+    end
+  end
+end
